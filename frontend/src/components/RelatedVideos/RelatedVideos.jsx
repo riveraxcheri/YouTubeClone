970f0d6afd5147251fbe8e26bc27fdd864de6a2c
riveraxcheri <117052183+riveraxcheri@users.noreply.github.com>
2023-03-24 04:14:31 -0500

    Started formating VideoPage.jsx and RelatedVideos.jsx

--- frontend/src/components/RelatedVideos/RelatedVideos.jsx
+++ frontend/src/components/RelatedVideos/RelatedVideos.jsx
@@ -1,7 +1,31 @@
-const RelatedVideos = () => {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const RelatedVideos = (props) => {
+    const [relatedVideos, setRelatedVideos] = useState([]);
+
+    async function fetchRelatedVideos(){
+        let response = await axios.get('https://www.googleapis.com/youtube/v3/search?relatedToVideoId={VIDEO ID HERE}&type=video&key={API KEY HERE}');
+        setRelatedVideos(response.data.results);
+    }
+
+    function mapRelatedVideos(){
+        return relatedVideos.map()
+    }
+
+    useEffect(()=> {
+        let mounted = true;
+        if(mounted){
+            fetchRelatedVideos();
+        }
+        return ()=> mounted = false;
+    }, [])
+
     return ( 
         <div>
-            
+            <h1>Related Videos</h1>
+
         </div>
      );
 }
--- frontend/src/pages/VideoPage/VideoPage.jsx
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -1,7 +1,13 @@
+import RelatedVideos from "../../components/RelatedVideos/RelatedVideos";
+import VideoPlayer from "../../components/VideoPlayer";
+import CommentList from "../../components/CommentList";
+
 const VideoPage = () => {
     return ( 
         <div>
-            
+            <VideoPlayer/>
+            <CommentList/>
+            <RelatedVideos/>
         </div>
      );
 }
